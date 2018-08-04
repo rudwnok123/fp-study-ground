2284b43cc202b65e38483e03483263780befe441
rudwnok123 <rudwnok123@naver.com>
2018-08-04 19:13:21 +0900

    merge

--- impl-func/common.js
+++ impl-func/common.js
@@ -9,37 +9,28 @@ $combo_item.addEventListener('click',function(e){
   console.log('선택한 숫자 : '+start_dan);
   //each
   // _each(start_dan);
-  each(start_dan);
+  _each(start_dan, end_dan);
   //filter
 
   //map
 })
 
-// function _curryr(fn) {
-//   return function(a, b) {
-//     return arguments.length == 2 ? fn(a, b) : function(b) { return fn(b, a); };
-//   }
-// }
-
-// function _is_object(obj){
-//   return typeof obj == 'object' && !!obj;
-// }
-
-// function _keys(obj){
-//   //obj라 객체 또는 배열인지
-//   return _is_object(obj) ? Object.keys(obj) : [];
-// }
-function each(a){
-  console.log(a * end_dan);
-}
-function _each(list){
-  // var keys = _keys(list);
-  var keys = list;git
-  // for ( var i =0; i< keys.length; i++){
-  // }
-    // console.log(iter(list[keys[i]], keys[i]));
-  // return keys * end_dan;
-  console.log(keys * end_dan)
-  return keys * end_dan;
+function _each(list,iter){
+  for(var i 0;i<list.length; i++){
+    iter(list[i]);
+  }
+  return list;
 }
 
+function filter(users,predi){
+  var new_list = [];
+  for(var i = 0; i<users.length; i++){
+    if(predi(users[i])){
+      new_list.push(users[i]);
+    }
+  }
+  return new_list;
+}
+console.log(
+    filter([1,2,3,4],function(num) { return num % 2;})
+)
